54af7257bda58bba553dd451fb7fcb6c137c6bf9
Danny Hayes <daniel.hayes@digital.cabinet-office.gov.uk>
2021-12-14 17:31:20 +0000

    PYI-459: Send passport details in POST request to the api

--- src/app/controllers/done.js
+++ src/app/controllers/done.js
@@ -1,18 +1,24 @@
+const axios = require("axios");
 const BaseController = require("hmpo-form-wizard").Controller;
+
+const { API_BASE_URL, API_VALIDATE_PASSPORT_PATH } = require("../../lib/config");
 
 class DoneController extends BaseController {
   async saveValues(req, res, next) {
     const attributes = {
       passportNumber: req.sessionModel.get("passportNumber"),
       surname: req.sessionModel.get("surname"),
-      forenames: req.sessionModel.get("givenNames"),
+      forenames: [req.sessionModel.get("givenNames")],
       dateOfBirth: req.sessionModel.get("dateOfBirth"),
-      issueDate: req.sessionModel.get("issueDate"),
       expiryDate: req.sessionModel.get("expiryDate"),
     };
 
-    // eslint-disable-next-line no-console
-    console.log(JSON.stringify(attributes, null, 2));
+    try {
+      const response = await axios.post(`${API_BASE_URL}${API_VALIDATE_PASSPORT_PATH}`, attributes);
+    } catch (error) {
+      res.error = error.name;
+      return next(error);
+    }
 
     super.saveValues(req, res, next);
   }
---
+++ src/lib/config.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const cfenv = require("cfenv");
+const appEnv = cfenv.getAppEnv();
+const serviceConfig = {};
+
+if (!appEnv.isLocal) {
+  serviceConfig.criPassportBackAPIUrl = appEnv.getServiceURL("cri-passport-back-api");
+}
+
+module.exports = {
+  API_BASE_URL: serviceConfig.coreBackAPIUrl || process.env.API_BASE_URL,
+  API_VALIDATE_PASSPORT_PATH: "/passport",
+  EXTERNAL_WEBSITE_HOST: process.env.EXTERNAL_WEBSITE_HOST,
+  PORT: process.env.PORT || 3000,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+};
