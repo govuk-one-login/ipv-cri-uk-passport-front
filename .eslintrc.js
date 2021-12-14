83e1d7e8101bf03c603f7a6ff3056df918e05888
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2021-12-14 09:37:36 +0000

    PYI-379: add minimal webserver

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
   env: {
     node: true,
+    es6: true,
+    es2020: true,
   },
   root: true,
-  extends: [
-    "eslint:recommended",
-    "prettier"
-  ],
+  extends: ["eslint:recommended", "prettier"],
   rules: {
     "no-console": 2,
     "padding-line-between-statements": [
