8d29c938efdf04f2cf8f21bb95678dbd800ed388
LaiTang-GDS <lai.tang@digital.cabinet-office.gov.uk>
2022-01-31 12:39:12 +0000

    code formatting

--- src/app/oauth2/middleware.js
+++ src/app/oauth2/middleware.js
@@ -17,7 +17,7 @@ module.exports = {
 
   parseSharedAttributesJWT: async (req, res, next) => {
     const requestJWT = req.query.request;
-    const headers = {'client_id': req.session?.authParams?.client_id }
+    const headers = { 'client_id': req.session?.authParams?.client_id };
 
     if (requestJWT) {
       const apiResponse = await axios.post(`${API_BASE_URL}${API_JWT_VERIFICATION_PATH}`, requestJWT, {headers: headers});
