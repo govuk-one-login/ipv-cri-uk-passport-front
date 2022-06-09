3c20396bcec25173b0cc48f93f33091753f11a65
Derren Wilson <derren.wilson@digital.cabinet-office.gov.uk>
2022-06-09 10:27:42 +0100

    [PYIC-1222] remove cookie banner code and reflect nunjucks usage in core-front

--- src/app/passport/controllers/root.js
+++ src/app/passport/controllers/root.js
@@ -2,7 +2,8 @@ const {Controller: BaseController} = require("hmpo-form-wizard");
 
 class RootController extends BaseController {
   async saveValues(req, res, next) {
-    const sharedClaims = req.session.JWTData.shared_claims;
+    //const sharedClaims = req.session.JWTData.shared_claims;
+    const sharedClaims = 'testing';
     if (sharedClaims) {
       if (sharedClaims?.names?.length > 0) {
         req.journeyModel.set("surname", sharedClaims.names[0]?.familyName)
