70335449f5b0f6d2e6f47176ff682dd681720236
Derren Wilson <derren.wilson@digital.cabinet-office.gov.uk>
2022-06-09 11:08:49 +0100

    [PYIC-1222] output error pages using the standard nunjucks process

--- src/app/passport/controllers/root.js
+++ src/app/passport/controllers/root.js
@@ -2,8 +2,8 @@ const {Controller: BaseController} = require("hmpo-form-wizard");
 
 class RootController extends BaseController {
   async saveValues(req, res, next) {
-    //const sharedClaims = req.session.JWTData.shared_claims;
-    const sharedClaims = 'testing';
+    const sharedClaims = req.session.JWTData.shared_claims;
+
     if (sharedClaims) {
       if (sharedClaims?.names?.length > 0) {
         req.journeyModel.set("surname", sharedClaims.names[0]?.familyName)
