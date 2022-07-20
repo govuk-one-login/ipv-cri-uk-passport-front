f64cedd709ddc8fd0028daab47b38166517c9a13
LaiTang-GDS <lai.tang@digital.cabinet-office.gov.uk>
2022-07-20 14:52:22 +0100

    PYIC-1646 implemented logic to respond to radio button selections

--- src/app/passport/controllers/prove-another-way.js
+++ src/app/passport/controllers/prove-another-way.js
@@ -1,16 +1,31 @@
 const BaseController = require("hmpo-form-wizard").Controller;
+const axios = require("axios");
+const {
+  API_BUILD_CLIENT_OAUTH_RESPONSE_PATH,
+  API_BASE_URL,
+} = require("../../../lib/config");
 
 class ProveAnotherWayController extends BaseController {
   async saveValues(req, res, next) {
     try {
       const action = req.form.values.proveAnotherWayRadio;
 
+      const headers = {
+        passport_session_id: req.session.passportSessionId,
+      };
+
       switch (action) {
         case "proveAnotherWay": {
-          break;
-        }
-        case "retry": {
-          break;
+          const apiResponse = await axios.post(
+            `${API_BASE_URL}${API_BUILD_CLIENT_OAUTH_RESPONSE_PATH}`,
+            undefined,
+            { headers: headers }
+          );
+
+          const redirect_url = apiResponse?.data?.client?.redirectUrl;
+          req.sessionModel.set("redirect_url", redirect_url);
+          super.saveValues(req, res);
+          return "/oauth2/callback";
         }
       }
     } catch (err) {
